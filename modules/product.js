51ab7f76b35b41e8744be02767fe665bf2ddeabe
unknown <asunov.artur.2007@gmail.com>
2022-12-10 21:04:37 +0300

    f

--- modules/product.js
+++ modules/product.js
@@ -330,7 +330,7 @@ class product{
         }
         //authValidate(access, 1111);
         let options = new chrome.Options();
-        options.addArguments(['--no-sandbox', '--headless']);
+        options.addArguments(['--no-sandbox', '--headless', '—disable-gpu']);
         let driver = new webdriver.Builder()
             .forBrowser('chrome')
             .setChromeOptions(options)
